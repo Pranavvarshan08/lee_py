2294d2d85f559e7b940172e7fdfd03f52aa689dc
Pranavvarshan08 <pranavvarshan26112003@gmail.com>
2026-08-01 22:11:37 +0530

    solved leetcode problem 486.py

---
+++ Easy489_predict_the_winner.py
@@ -0,0 +1,16 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+
+        @lru_cache(None)
+        def dp(left,right):
+            if left == right:
+                return nums[left]
+            
+            pick_left=nums[left]-dp(left+1,right)
+            pick_right=nums[right]-dp(left,right-1)
+
+            return max(pick_left,pick_right)
+
+        return dp(0,len(nums)-1)>=0
+
+
