acb0b07f107db5fd93f9a89b24db103bfb7b5f9d
Pranavvarshan08 <pranavvarshan26112003@gmail.com>
2026-08-27 20:34:13 +0530

    solved leetcode problem.py

---
+++ 17.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return[]
+
+        phone={
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+
+        }
+        ans=[]
+
+        def backtrack(i,path):
+            if i == len(digits):
+                ans.append("".join(path))
+                return
+            for char in phone[digits[i]]:
+                path.append(char)
+                backtrack(i+1,path)
+                path.pop()
+    
+        backtrack(0,[])
+        return ans
+
+            
+        
