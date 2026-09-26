fa35b507130afe7ed250554abc7967cdfab6b632
Pranavvarshan08 <pranavvarshan26112003@gmail.com>
2026-09-26 18:54:34 +0530

    solved leetcode problem.

---
+++ Medium/184.py
@@ -0,0 +1,12 @@
+select 
+d.name as Department,
+e.name as Employee,
+e.salary as Salary
+from Employee e
+join Department d 
+on e.departmentID=d.id
+where e.salary =(
+    select max(e2.salary)
+    from Employee e2
+    where e2.departmentId =e.departmentId
+)
